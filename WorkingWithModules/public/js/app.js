f935038806c64e04d550c11f75e1f98dfc42c8e3
Chayan Adhikari <adhikari.chayan@gmail.com>
2023-01-30 00:14:09 +0530

    JS Modules demo

---
+++ WorkingWithModules/public/js/app.js
@@ -0,0 +1,27 @@
+import getSessions, {sessionTemplate, sessionUrl} from './sessionRepository.js';
+//import {sessionTemplate, errorMessage} from './template.js';
+//import * as template from '/template,js'
+
+function render() {
+  var list = document.querySelector('#sessions');
+  if (!list) return;
+  //list.innerHTML = template.sessionTemplate(data.listItems);
+  list.innerHTML = sessionTemplate(data.listItems);
+};
+
+
+var data = {
+  listItems: []
+};
+
+getSessions()
+  .then((sessions) => {
+    console.log('promises!')
+    data.listItems = sessions;
+    render();
+  });
+
+
+
+
+
